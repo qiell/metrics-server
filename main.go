da957df57521da87528b109129cae07fbcc2ac4f
Akash Srivastava <akashsrivastava4927@gmail.com>
2020-10-18 22:40:55 +0530

    Add support to store the metrics into store via metricsHandler
    Signed-off-by: Akash Srivastava <akashsrivastava4927@gmail.com>

--- main.go
+++ main.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"encoding/json"
 	"github.com/qiell/metrics-server/constants"
+	"github.com/qiell/metrics-server/store"
+	"github.com/qiell/metrics-server/store/inmemory"
 	"log"
 	"net/http"
 )
+
+func init() {
+	store.Object = inmemory.New()
+}
 
 func main() {
 	// add metrics route
@@ -22,7 +29,16 @@ func metricsHandler(w http.ResponseWriter, r *http.Request) {
 	// if request method is of type POST, then process the request,
 	// otherwise return 500
 	if r.Method == constants.Post {
-		w.Write([]byte("request successful"))
+		// create object of Metrics
+		var metrics store.Metrics
+		// decode request body into metrics object
+		err := json.NewDecoder(r.Body).Decode(&metrics)
+		if err != nil {
+			http.Error(w, "Error in parsing request body", http.StatusInternalServerError)
+			return
+		}
+		// add metrics to Store
+		store.Object.Add(r.Host, &metrics)
 		return
 	} else {
 		http.Error(w, "Invalid request method", http.StatusInternalServerError)
--- store/types.go
+++ store/types.go
@@ -2,7 +2,7 @@ package store
 
 // Metrics struct will be used to parse the metrics json response of a client
 type Metrics struct {
-	CPU int `json:"percentage_cpu_used"`
+	CPU    int `json:"percentage_cpu_used"`
 	Memory int `json:"percentage_memory_used"`
 }
 
